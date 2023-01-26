5c127280eae0261fbe46017946a8a609b09ff2f6
Gilan Suleimenov <gilan.suleimenov@gmail.com>
2023-01-26 11:41:31 +0300

    Добавлен файл фикстуры

---
+++ conftest.py
@@ -0,0 +1,12 @@
+import pytest
+
+from main import BooksCollector
+
+
+@pytest.fixture
+def book():
+    collector = BooksCollector()
+    collector.add_new_book('Гордость и предубеждение и зомби')
+    collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+
+    return collector
